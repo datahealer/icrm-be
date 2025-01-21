313c3f00ee973004545932d432a5e009a3754baf
Hardik Garg <hardik@inzint.com>
2025-01-21 16:19:08 +0530

    fix cookie

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -223,6 +223,7 @@ export const loginPerson = async (req, res) => {
         secure: true,
         sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000,
+        domain: 'https://main.d2q0ys24czjejd.amplifyapp.com/'
       });
 
       await person.save();
