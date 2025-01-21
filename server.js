83da78b0144e35e87c107d69ef5c6b1af38b1f50
Hardik Garg <hardik@inzint.com>
2025-01-21 16:01:47 +0530

    fix cors error

--- server.js
+++ server.js
@@ -25,10 +25,22 @@ connect();
 
 // Middlewares
 const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: 'https://main.d2q0ys24czjejd.amplifyapp.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  credentials: true, // Allow cookies and Authorization headers
 };
+
 app.use(cors(corsOptions));
+
+// Explicitly handle preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://main.d2q0ys24czjejd.amplifyapp.com');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Instead of bodyParser.urlencoded
 app.use(cookieParser());
