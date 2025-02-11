30295f19e1279bc9aa0cd36972ed75857ce3c77e
Hardik Garg <hardik@inzint.com>
2025-02-11 16:01:55 +0530

    edit env var name

--- controllers/s3Controller.js
+++ controllers/s3Controller.js
@@ -1,14 +1,14 @@
 import AWS from "aws-sdk";
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: "ap-south-1",
 });
 export const generateUploadUrl = async (req, res) => {
   const { fileName, folder } = req.body;
   const key = `${folder}/${Date.now()}-${fileName}`;
   const params = {
-    Bucket: process.env.AWS_BUCKET,
+    Bucket: process.env.BUCKET_NAME,
     Key: key,
     ContentType: "image/*",
     Expires: 60 * 5,
